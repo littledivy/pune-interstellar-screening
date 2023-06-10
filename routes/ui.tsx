ccf580c6e63b245c674ef1e4e21f3670944977da
Divy Srivastava <dj.srivastava23@gmail.com>
2023-06-10 21:58:38 +0530

    Updates

--- routes/ui.tsx
+++ routes/ui.tsx
@@ -7,6 +7,11 @@ export const kv = await Deno.openKv();
 const PRICE_PER_SEAT = 550 + 25;
 
 const removedSeats = [
+  // O 11 - 24
+  // N 11 - 24
+  ..."O".repeat(14).split("").map((c, i) => `${c}${i + 11}`),
+  ..."N".repeat(14).split("").map((c, i) => `${c}${i + 11}`),
+
   // M 12 - 24
   ..."M".repeat(13).split("").map((m, i) => `${m}${i + 12}`),
   // L 21 - 24
